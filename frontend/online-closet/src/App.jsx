c04018c40f18beb6dfe0ba5033a1076cb6248239
Joshua Casuga <jomatthew8@gmail.com>
2025-03-12 22:46:54 -0700

    tabs on homepage

--- frontend/online-closet/src/App.jsx
+++ frontend/online-closet/src/App.jsx
@@ -2,11 +2,12 @@ import { Box } from "@chakra-ui/react"
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx';
 import ProfilePage from "./pages/ProfilePage.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
     <Box minH={"100vh"}>
-      {/* <Navbar /> */}
+      <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/user/:id" element={<ProfilePage/>}/>
--- frontend/online-closet/src/pages/HomePage.jsx
+++ frontend/online-closet/src/pages/HomePage.jsx
@@ -1,8 +1,34 @@
+import { Container, Flex, Tabs } from '@chakra-ui/react'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDir={{
+          base:"column"
+        }}
+      >
+        <Tabs.Root 
+          defaultValue="home"
+          size="lg"
+        >
+          <Tabs.List>
+            <Tabs.Trigger value="clothes">
+              Clothes
+            </Tabs.Trigger>
+            <Tabs.Trigger value="outfits">
+              Saved Outfits
+            </Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="clothes">Save pieces</Tabs.Content>
+          <Tabs.Content value="outfits">Save outfits</Tabs.Content>
+        </Tabs.Root>
+      </Flex>
+    </Container>
   )
 }
 
