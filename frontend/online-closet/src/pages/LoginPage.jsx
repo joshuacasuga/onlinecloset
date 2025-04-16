e82addf3638595361ad30a1ead603fc149dc348b
Joshua Casuga <jomatthew8@gmail.com>
2025-04-15 23:12:44 -0700

    update user login/registration

--- frontend/online-closet/src/pages/LoginPage.jsx
+++ frontend/online-closet/src/pages/LoginPage.jsx
@@ -1,9 +1,13 @@
 import { Container, Flex, Box, Input, Button, Stack } from '@chakra-ui/react'
-//import { PasswordInput } from "@components/ui/password-input"
 import React, { useState } from 'react'
 
 const LoginPage = () => {
     const [isLogin, setIsLogin] = useState(true);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     
     const toggleForm = () => setIsLogin(!isLogin);
 
@@ -19,7 +23,7 @@ const LoginPage = () => {
             <Box
             background="#4c7d7d"
             width="500px"
-            height="500px"
+            height="600px"
             padding="4"
             color="white"
             flexDir={{
@@ -33,24 +37,44 @@ const LoginPage = () => {
                         Password
                         <Input type="password"/>
                         <Button>Login</Button>
-                        Don't have an account?
-                        <Button variant="link" onClick={toggleForm}>Register</Button>
+                        <div>
+                            Don't have an account?
+                            <Button variant="link" onClick={toggleForm}>Register</Button>
+                        </div>
                     </Stack>
                 ) : (
                     <Stack spacing={4}>
                         First Name
-                        <Input/>
+                        <Input
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            type="text"/>
                         Last Name
-                        <Input/>
+                        <Input
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            type="text"/>
                         Email
-                        <Input/>
+                        <Input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"/>
                         Username
-                        <Input/>
+                        <Input
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            type="username"/>
                         Password
-                        <Input type="password"/>
+                        <Input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"/>
                         <Button>Register</Button>
-                        Already have an account?
-                        <Button variant="link" onClick={toggleForm}>Login</Button>
+                        <div>
+                            Already have an account?
+                            <Button variant="link" onClick={toggleForm}>Login</Button>
+                        </div>
+                        
                     </Stack>
                 )}
             </Box>
